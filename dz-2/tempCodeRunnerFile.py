493ad35c49740bc3a092fe1da0fc3af1f7f10f52
FeigMary <feigmary@gmail.com>
2023-09-28 11:23:32 +0300

    task1

---
+++ dz-2/tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+number = int(input("Введите целое число в десятичной системе: "))
+print(f'Встроенная функция hex = {hex(number)}')
+
+def to_hex(number):
+    hex_dec_rule = "0123456789ABCDEF"
+    hex_str = " "
+    
+    while number > 0:
+        hex_str = hex_dec_rule[number % 16] + hex_str
+        number = number // 16
+    return '0x'+ hex_str
+
+print(f'Собственная функция = {to_hex(number)}')
