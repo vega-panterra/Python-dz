da94ea2d0f62509c75ceb4f69e4ad3102ed205f3
FeigMary <feigmary@gmail.com>
2023-09-28 11:23:48 +0300

    task1

--- dz-2/task1.py
+++ dz-2/task1.py
@@ -1,3 +1,16 @@
 # Напишите программу, которая получает целое число и возвращает его шестнадцатеричное строковое представление.
 # Функцию hex используйте для проверки своего результата.
 
+number = int(input("Введите целое число в десятичной системе: "))
+print(f'Встроенная функция hex = {hex(number)}')
+
+def to_hex(number):
+    hex_dec_rule = "0123456789ABCDEF"
+    hex_str = " "
+
+    while number > 0:
+        hex_str = hex_dec_rule[number % 16] + hex_str
+        number = number // 16
+    return '0x'+ hex_str
+
+print(f'Собственная функция = {to_hex(number)}')
