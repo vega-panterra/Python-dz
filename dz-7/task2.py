5aff4d228e56a5398dee008794e8f2608e00dfc2
FeigMary <feigmary@gmail.com>
2023-10-16 13:44:50 +0300

    tasks

--- dz-7/task2.py
+++ dz-7/task2.py
@@ -1,33 +1,26 @@
-# Напишите функцию группового переименования файлов. Она должна:
-# - принимать параметр желаемое конечное имя файлов. При переименовании в конце имени добавляется порядковый номер.
-# - принимать параметр количество цифр в порядковом номере.
-# - принимать параметр расширение исходного файла. Переименование должно работать только для этих файлов внутри каталога.
-# - принимать параметр расширение конечного файла.
-# - принимать диапазон сохраняемого оригинального имени. Например для диапазона [3, 6] берутся буквы с 3 по 6 из исходного имени файла.
-# К ним прибавляется желаемое конечное имя, если оно передано. Далее счётчик файлов и расширение.
-
-import os
-
-__all__ = ['group_files']
-
-def group_files(desired_name, digit_count, source_extension, target_extension, name_range):
-    directory = os.getcwd()
-    file_counter = 1
-
-    for filename in os.listdir(directory):
-        if filename.endswith(source_extension):
-            original_name = filename[name_range[0]-1:name_range[1]]
-            if desired_name:
-                original_name += desired_name
-
-            number = str(file_counter).zfill(digit_count)
-
-            new_name = original_name + number + target_extension
-
-            os.rename(os.path.join(directory, filename), os.path.join(directory, new_name))
-
-            file_counter += 1
+# Напишите функцию, которая генерирует псевдоимена.
+# Имя должно начинаться с заглавной буквы, состоять из 4-7 букв, среди которых обязательно должны быть гласные.
+# Полученные имена сохраните в файл.
 
 
-if __name__=="__main__":
-    group_files("_new", 2, ".txt", ".jpg", [3, 6])
+from random import randint
+VOWELS = "AEYUIOaeuioy"
+
+def rnd_name():
+    name_len = randint(4, 7)
+    while True:
+        name = ""
+        for i in range(name_len):
+            name += chr(randint(65, 90))
+        for i in name:
+            if i in VOWELS:
+                return name.capitalize()
+
+
+def rnd_name_in_file(count, file_name):
+    with open(file_name, "a") as f:
+        for i in range(count):
+            f.write(rnd_name()+"\n")
+
+
+rnd_name_in_file(5, r"dz-7\names.txt")
