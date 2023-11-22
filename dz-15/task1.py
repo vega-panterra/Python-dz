ca3b714572e77e0112aec9e492d9b4bd6342adcf
FeigMary <feigmary@gmail.com>
2023-11-22 11:38:48 +0300

    task2

--- dz-15/task1.py
+++ dz-15/task1.py
@@ -1,14 +1,15 @@
 import logging
-logging.basicConfig(filename='logs.log',filemode='w',encoding='utf-8',level=logging.ERROR)
+
+logging.basicConfig(filename='logs.log', level=logging.INFO, encoding='utf-8',format='%(asctime)s - %(levelname)s - %(message)s')
 
 def my_func(storage, key, value=None):
     try:
         return storage[key]
     except KeyError:
-        logging.error(f'отсутствует ключ {key} в словаре {storage}')
+        logging.error(f"Отсутствует ключ {key} в словаре {storage}")
         return value
 
 f={'f':'1', 'd':'3', 's':'5'}
 print (my_func (f, key='a', value=2))
 
-# ERROR:root:отсутствует ключ a в словаре {'f': '1', 'd': '3', 's': '5'}
+# 2023-11-22 11:38:17,261 - ERROR - Отсутствует ключ a в словаре {'f': '1', 'd': '3', 's': '5'}
---
+++ dz-15/task2.py
@@ -0,0 +1,36 @@
+import logging
+
+logging.basicConfig(filename='logs.log', level=logging.INFO, encoding='utf-8',format='%(asctime)s - %(levelname)s - %(message)s')
+
+class InvalidTextError(Exception):
+    pass
+
+class InvalidNumberError(Exception):
+    pass
+
+class Archive:
+    def __init__(self, text, number):
+        if not isinstance(text, str) or not text:
+            raise InvalidTextError(f"Неверный текст: {text} или пустая строка.")
+
+        if not (isinstance(number, int) and number > 0) and not (isinstance(number, float) and number > 0):
+            logging.error(f"Ввод должен быть числового формата: {number}")
+            raise InvalidNumberError(f"Неверный номер: {number} или число должно быть положительным целым / число с плавающей запятой.")
+
+        self.text = text
+        self.number = number
+
+    def __str__(self):
+        logging.info(f"Текст {self.text} и номер {self.number}")
+        return f"Текст {self.text} и номер {self.number}."
+
+try:
+    archive1 = Archive(42, 42)
+    print(archive1)
+
+except InvalidTextError as e:
+    logging.error(f"Ввод текстового формата: {e}")
+    print(f"Ввод текстового формата: {e}")
+except TypeError as e:
+    logging.error(f"Ввод числового формата: {e}")
+    print(f"Ввод числового формата: {e}")
