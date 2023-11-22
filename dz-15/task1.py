8144cb4041821ceea7295eb78e53a108c3b20ca0
FeigMary <feigmary@gmail.com>
2023-11-22 11:31:56 +0300

    task1

--- dz-15/task1.py
+++ dz-15/task1.py
@@ -1,41 +1,12 @@
 import logging
-import argparse
+logging.basicConfig(filename='logs.log',filemode='w',encoding='utf-8',level=logging.ERROR)
 
+def my_func(storage, key, value=None):
+    try:
+        return storage[key]
+    except KeyError:
+        logging.error(f'отсутствует ключ {key} в словаре {storage}')
+        return value
 
-logging.basicConfig(filename='logs.log', level=logging.INFO, encoding='utf-8', format='%(asctime)s - %(levelname)s - %(message)s')
-
-
-class Archive:
-    def __init__(self, text, number):
-        if not isinstance(text, str) or not text:
-            raise TypeError(f"Неверный текст: {text}. Текст должен быть непустой строкой.")
-
-        if not isinstance(number, (int, float)) or not number:
-            raise TypeError(f"Неверный номер: {number}. Число должно быть положительным целым числом или числом с плавающей запятой.")
-
-        self.text = text
-        self.number = number
-
-    def __str__(self):
-        logging.info(f"Текст {self.text} и номер {self.number}")
-        return f"Текст {self.text} и номер {self.number}."
-
-def main():
-    parser = argparse.ArgumentParser(description="Создать экземпляр архива")
-    parser.add_argument("--text", type=str, help="Текст для архива")
-    parser.add_argument("--number", type=float, help="Номер для архива")
-    args = parser.parse_args()
-
-    if args.text is not None and args.number is not None:
-        try:
-            archive = Archive(args.text, args.number)
-            print(archive)
-        except Exception as e:
-            logging.error(f"Ввод должен быть текстового формата: {e}")
-            print(f"Ввод должен быть текстового формата: {e}")
-        except Exception as e:
-            logging.error(f"Ввод должен быть числового формата: {e}")
-            print(f"Ввод должен быть числового формата: {e}")
-
-if __name__ == "__main__":
-    main()
+f={'f':'1', 'd':'3', 's':'5'}
+print (my_func (f, key='a', value=2))
